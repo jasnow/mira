2e32186e9a185f7abce84c48c3dfec30b3e8d1bc
Al Snow <jasnow@hotmail.com>
2016-07-20 17:28:42 -0400

    Upgraded to Rails 5.0

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 20151030073834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "datapackage_resource_fields", force: :cascade do |t|
     t.integer  "datapackage_resource_id"
     t.text     "name"
